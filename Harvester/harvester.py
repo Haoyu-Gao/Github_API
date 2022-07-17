5bac4816d0d8bdc6ff99cb451fcadd05c142acf0
Haoyu Gao <80379456+Haoyu-Gao@users.noreply.github.com>
2022-07-17 11:16:45 +1000

    modify files and structure, use multiple tokens for harvesting.

--- Harvester/harvester.py
+++ Harvester/harvester.py
@@ -1,5 +1,5 @@
 from github import Github, RateLimitExceededException
-from credentials import ACCESS_TOKEN, DB_USER, DB_PASSWORD, URL
+from credentials import ACCESS_TOKEN_0, ACCESS_TOKEN_1, DB_USER, DB_PASSWORD, URL
 import time
 from db_client import DBClient
 from urllib.request import urlopen
@@ -17,39 +17,60 @@ with open("External.txt", 'r') as f:
         simplification_set.add(line.strip())
 
 
-def github_api_data_harvester(db_name):
+def github_api_data_harvester(db_name, id):
+
+    ACCESS_TOKEN = ACCESS_TOKEN_0 if id == 0 else ACCESS_TOKEN_1
 
     g = Github(ACCESS_TOKEN)
     repos = g.get_repos()  # this is all the available public repos sorted based on their created time
-    while True:
 
+    count = 0
+    while True:  # this is a long-running process, gather a few days of data and decide what to do next.
         try:
+            print("count", count)
             last_md_url = None
-            for repo in repos:
+            page = count * 4 + id
+            repo_page = repos.get_page(page)
+
+            i = 0
+            while i < len(repo_page):
+
                 try:
+                    repo = repo_page[i]
                     commits = repo.get_commits()  # the commits in a repo are also sorted based on time.
+                    print("page idx", i)
+                    j = 0
+                    while j < commits.totalCount:
+                        try:
+                            print("commit count", j)
+                            if last_md_url is None:
+                                for file in commits[j].files:
+                                    if file.filename == "README.md":
+                                        last_md_url = file.raw_url
 
-                    for i in range(commits.totalCount):
-                        if last_md_url is None:
-                            for file in commits[i].files:
-                                if file.filename == "README.md":
-                                    last_md_url = file.raw_url
-
-                        else:
-                            current_commit_message = commits[i].commit.message
-                            if only_change_md_file(commits, i) and is_simplification_commit(current_commit_message):
-                                gather_md_file_pairs(db_name, repo, commits, i, last_md_url)
-                                last_md_url = commits[i].files[0].raw_url
-
+                            else:
+                                current_commit_message = commits[j].commit.message
+                                if only_change_md_file(commits, j) and is_simplification_commit(current_commit_message):
+                                    gather_md_file_pairs(db_name, repo, commits, j, last_md_url)
+                                    last_md_url = commits[j].files[0].raw_url
+                        except RateLimitExceededException as e:
+                            print(e)
+                            continue
+                        j += 1
+                    i += 1
                 except RateLimitExceededException as e:
                     print("Exceed the rate limit")
                     time.sleep(300)
                     continue
+
+                count += 1
+
         except RateLimitExceededException as e:  # Github API only have a rate limit of 5000 requests/ hour. 
             print("Exceed the rate limit")
             time.sleep(300)  # sleep for 5 minutes to avoid the time limits.
             continue
         except Exception as e:
+            print(e)
             continue
 
 
@@ -69,8 +90,8 @@ def is_simplification_commit(current_commit_message):
     """
     tokenizer = nltk.SpaceTokenizer()
     tokens = tokenizer.tokenize(current_commit_message)
-    print(simplification_set)
-    print(tokens)
+    # print(simplification_set)
+    # print(tokens)
     for token in tokens:
         if token in simplification_set:
             return True
@@ -104,7 +125,9 @@ def gather_md_file_pairs(db_name, repo, commits, idx, last_md_url):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--db", type=str, help="Specify the intended db name")
+    parser.add_argument("--id", type=int, help="Specify the currently working process id")
     args = parser.parse_args()
     db_name = args.db
-    github_api_data_harvester(db_name)
+    id = args.id
+    github_api_data_harvester(db_name, id)
 
--- credentials.py
+++
@@ -1,7 +0,0 @@
-
-ACCESS_TOKEN = "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"
-IP = "127.0.0.1"
-PORT = 5984
-DB_USER = "admin"
-DB_PASSWORD = "password1234"
-URL = 'http://localhost:5984/'
